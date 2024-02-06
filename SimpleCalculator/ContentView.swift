a7a91f6c5d3925bfc9aaa6efb5d3a16bd7ec9183
umpu <carol.ledgers0h@icloud.com>
2024-02-06 15:32:02 +0500

    Initial Commit

---
+++ SimpleCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleCalculator
+//
+//  Created by Andrei Gorbunov on 06.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Andrei Gorbunov on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
